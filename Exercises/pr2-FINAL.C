25ced947541d652a868fa6f5a3be2202fe0ca38e
cristian ayala <cristian.adrian1993@gmail.com>
2023-11-25 16:13:02 -0300

    Se realiza ajuste al código de la PR2 y agrego comentarios

--- Exercises/pr2-FINAL.C
+++ Exercises/pr2-FINAL.C
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+//Defino las constantes
 #define NUM_SUPPORTERS 3
 #define BASE_MEMBERSHIP_YEARS 5
 #define SILVER_MEMBERSHIP_YEARS 10
@@ -10,10 +11,12 @@
 #define GOLD_MEMBERSHIP_POINTS 20
 #define RECORDS_PENALTY_POINTS 5
 
+//Defino los tipos de membresía (mismos que la PR1)
 typedef enum {
     BASE, SILVER, GOLD
 } tMembershipType;
 
+//Defino la estructura para guardar la información del SUPPORTER (tupla)
 typedef struct {
     char name[26];
     int age;
@@ -22,15 +25,15 @@ typedef struct {
     tMembershipType membershipType;
 } tSupporter;
 
+//Defino la función para copiar de un SUPPORTER a otro (tupla->tupla) 
 void supporterCpy(tSupporter *supporterDst, tSupporter supporterSrc) {
     strcpy(supporterDst->name, supporterSrc.name);
     supporterDst->age = supporterSrc.age;
     supporterDst->membershipYears = supporterSrc.membershipYears;
     supporterDst->hasRecords = supporterSrc.hasRecords;
-    // Note: Assuming membershipType is also to be copied
-    supporterDst->membershipType = supporterSrc.membershipType;
 }
 
+//Defino función que determina el tipo de membresía según los años
 tMembershipType getMembershipType(int membershipYears) {
     if (membershipYears <= BASE_MEMBERSHIP_YEARS) {
         return BASE;
@@ -41,6 +44,26 @@ tMembershipType getMembershipType(int membershipYears) {
     }
 }
 
+int getPoints(tSupporter supporter) {
+    int points = 0;
+    
+    //Hago la asignación de la puntuación dependiendo el tipo de membresía
+    if (supporter.membershipType == BASE) {
+        points = BASE_MEMBERSHIP_POINTS;
+    } else if (supporter.membershipType == SILVER) {
+        points = SILVER_MEMBERSHIP_POINTS;
+    } else if (supporter.membershipType == GOLD) {
+        points = GOLD_MEMBERSHIP_POINTS;
+    }
+
+    //Agrego if para aplicar la penalización si tiene antecedentes
+    if (supporter.hasRecords) {
+        points -= RECORDS_PENALTY_POINTS;
+    }
+
+    return points;
+}
+//Defino función para obtener información del SUPPORT
 void readSupporter(tSupporter *supporter) {
     printf("NAME (25 CHAR MAX, NO SPACES)?\n");
     scanf("%25s", supporter->name);
@@ -51,40 +74,30 @@ void readSupporter(tSupporter *supporter) {
     printf("MEMBERSHIP YEARS (AN INTEGER)?\n");
     scanf("%d", &supporter->membershipYears);
     
+    int hasRecords;
     printf("HAS RECORDS (0-FALSE, 1-TRUE)?\n");
-    scanf("%d", (int *)&supporter->hasRecords);
-
+    scanf("%d", &hasRecords);
+    supporter->hasRecords = hasRecords;
     supporter->membershipType = getMembershipType(supporter->membershipYears);
 }
 
-int getPoints(tSupporter supporter) {
-    int points = 0;
-    switch (supporter.membershipType) {
-        case BASE: points = BASE_MEMBERSHIP_POINTS; break;
-        case SILVER: points = SILVER_MEMBERSHIP_POINTS; break;
-        case GOLD: points = GOLD_MEMBERSHIP_POINTS; break;
-    }
-
-    if (supporter.hasRecords) {
-        points -= RECORDS_PENALTY_POINTS;
-    }
-
-    return points;
-}
-
+//Se define esta función para mostrar por pantalla la información del SUPPORTER
 void writeSupporter(tSupporter supporter) {
     printf("NAME: %s\n", supporter.name);
     printf("AGE: %d\n", supporter.age);
     printf("MEMBERSHIP YEARS: %d\n", supporter.membershipYears);
     printf("HAS RECORDS (0-FALSE, 1-TRUE): %d\n", supporter.hasRecords);
-    printf("MEMBERSHIP TYPE (1-BASE, 2-SILVER, 3-GOLD): %d\n", supporter.membershipType);
+    printf("MEMBERSHIP TYPE (1-BASE, 2-SILVER, 3-GOLD): %d\n", supporter.membershipType + 1);
+    printf("POINTS: %d\n", getPoints(supporter));
 }
 
+//Acá inicia la lógica principal del programa
 int main() {
     tSupporter supporters[NUM_SUPPORTERS];
     tSupporter selectedSupporter;
     int i, maxPoints, currentPoints;
 
+    //Solicita al usuario que ingrese los datos de los seguidores
     printf("INPUT DATA\n");
     for (i = 0; i < NUM_SUPPORTERS; i++) {
         readSupporter(&supporters[i]);
@@ -93,6 +106,7 @@ int main() {
     selectedSupporter = supporters[0];
     maxPoints = getPoints(selectedSupporter);
 
+    //Se realiza iteración para identificar el SUPPORTER que tiene más puntos
     for (i = 1; i < NUM_SUPPORTERS; i++) {
         currentPoints = getPoints(supporters[i]);
         if (currentPoints > maxPoints ||
@@ -101,7 +115,7 @@ int main() {
             maxPoints = currentPoints;
         }
     }
-
+    //Se muestra por pantalla el SUPPORTER seleccionado
     printf("SELECTED SUPPORTER\n");
     writeSupporter(selectedSupporter);
 
